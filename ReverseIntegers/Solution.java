17347cfeb7bc73b1bd6b427d758194d73b95e829
VaibhaviDharashivkar97 <54762802+VaibhaviDharashivkar97@users.noreply.github.com>
2020-05-28 02:42:14 -0400

    Add files via upload

---
+++ ReverseIntegers/Solution.java
@@ -0,0 +1,23 @@
+public class Solution {
+
+        public static int reverse(int x) {
+            int result = 0;
+
+            while (x != 0)
+            {
+                int tail = x % 10;
+                int newResult = result * 10 + tail;
+                if ((newResult - tail) / 10 != result)
+                { return 0; }
+                result = newResult;
+                x = x / 10;
+            }
+
+            return result;
+        }
+
+    public static void main(String[] args) {
+        System.out.println(reverse(123));
+        System.out.println(reverse(1534236469));
+    }
+}
